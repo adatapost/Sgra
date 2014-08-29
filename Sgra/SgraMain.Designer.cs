47549c0295ae154f1e54ef5025a5b66ad457d858
Kanaiyalal Prajapati <adatapost@gmail.com>
2014-08-29 12:17:29 +0530

    added

---
+++ Sgra/SgraMain.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Sgra
+{
+    partial class SgraMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.kvOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.kvBtnImport = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // kvOpenFile
+            // 
+            this.kvOpenFile.FileName = "openFileDialog1";
+            // 
+            // kvBtnImport
+            // 
+            this.kvBtnImport.Location = new System.Drawing.Point(12, 12);
+            this.kvBtnImport.Name = "kvBtnImport";
+            this.kvBtnImport.Size = new System.Drawing.Size(99, 39);
+            this.kvBtnImport.TabIndex = 0;
+            this.kvBtnImport.Text = "Import";
+            this.kvBtnImport.UseVisualStyleBackColor = true;
+            // 
+            // SgraMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 358);
+            this.Controls.Add(this.kvBtnImport);
+            this.Name = "SgraMain";
+            this.Text = "Sapp";
+            this.Load += new System.EventHandler(this.SgraMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog kvOpenFile;
+        private System.Windows.Forms.Button kvBtnImport;
+    }
+}
+
---
+++ Sgra/SgraMain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Access;
+using System.Runtime.InteropServices;
+
+namespace Sgra
+{
+    public partial class SgraMain : System.Windows.Forms.Form
+    {
+        public SgraMain()
+        {
+            InitializeComponent();
+        }
+
+        private void SgraMain_Load(object sender, EventArgs e)
+        {
+            var databaseLocation = System.IO.Path.GetDirectoryName(System.IO.Path.GetDirectoryName(System.Windows.Forms.Application.StartupPath)) + @"\Sgra.accdb";
+            var query = "vehicle";
+            kvBtnImport.Click += (sa, ea) =>
+                {
+                    kvOpenFile.Filter = "Microsoft Excel (*.xlsx)|*.xlsx";
+
+                   // kvOpenFile.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads";
+                    if (kvOpenFile.ShowDialog() == DialogResult.OK)
+                    {
+                        var locationToExportTo = kvOpenFile.FileName;
+                        try
+                        {
+                            ImportAccessToExcelQuery(databaseLocation, query, locationToExportTo);
+                        }
+                        catch (Exception)
+                        {
+                            MessageBox.Show("Error: Invalid format of data."); 
+                        }
+                    }
+                };
+        }
+
+        private static void ExportAccessToExcelQuery(string databaseLocation, string queryNameToExport, string locationToExportTo)
+        {
+            var app = new Microsoft.Office.Interop.Access.Application();
+         
+            app.OpenCurrentDatabase(databaseLocation);
+            app.DoCmd.TransferSpreadsheet(AcDataTransferType.acExport, AcSpreadSheetType.acSpreadsheetTypeExcel12,
+                                                  queryNameToExport, locationToExportTo, true);
+            app.CloseCurrentDatabase();
+            app.Quit();
+            Marshal.ReleaseComObject(app);
+        }
+        private static void ImportAccessToExcelQuery(string databaseLocation, string queryNameToExport, string locationToExportTo)
+        {
+            var app = new Microsoft.Office.Interop.Access.Application();
+
+            app.OpenCurrentDatabase(databaseLocation);
+            app.DoCmd.TransferSpreadsheet(AcDataTransferType.acImport, AcSpreadSheetType.acSpreadsheetTypeExcel12,
+                                                  queryNameToExport, locationToExportTo, true);
+            app.CloseCurrentDatabase();
+            app.Quit();
+            Marshal.ReleaseComObject(app);
+        }
+        private void ExportData(string fileName)
+        {
+            var connectionString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=Excel 12.0;", fileName);
+
+            using (var adapter = new OleDbDataAdapter("SELECT * FROM [Sheet1$]", connectionString))
+            {
+                var dt = new System.Data.DataTable();
+                adapter.Fill(dt);
+            }
+
+        }
+    }
+}
